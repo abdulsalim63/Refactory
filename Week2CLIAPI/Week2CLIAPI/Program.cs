ffb3eacdf12300e4cdd96652052fe76dcd8ef82a
Gigaming Intermedia Solusindo <abdul.salim@gigaming.io>
2020-02-14 11:43:11 +0700

    update done undone

--- Week2CLIAPI/Week2CLIAPI/Program.cs
+++ Week2CLIAPI/Week2CLIAPI/Program.cs
@@ -83,34 +83,53 @@ namespace Week2CLIAPI
             }
             else if (todoMethod[1].ToLower() == "done")
             {
+                var requestList = new RestRequest(Method.GET);
+                requestList.AddHeader("Content-Type", "list/json");
+                var response = client.Execute(requestList);
+                var jsonFile = JsonConvert.DeserializeObject<List<Todo>>(response.Content);
+
+                var content = "";
+
+                foreach (var i in jsonFile)
+                {
+                    if (i.id.ToString() == todoMethod[2]) { content += i.todo; break; }
+                }
+
                 var client1 = new RestClient($"http://localhost:3000/todo/{todoMethod[2]}");
                 client1.Timeout = -1;
 
-                var request0 = new RestRequest(Method.GET);
-                var response0 = client1.Execute(request0);
-                var content1 = JsonConvert.DeserializeObject<List<Todo>>(response0.Content);
-                //var content = content1[0].todo;
-
                 var request = new RestRequest(Method.DELETE);
                 request.AddHeader("Content-Type", "application/json");
-                IRestResponse response = client1.Execute(request);
+                client1.Execute(request);
 
-                //Console.WriteLine($"{{\n\t\"todo\" : \"{content}\"\n}}");
+                Console.WriteLine($"{{\n\t\"todo\" : \"{content}\"\n}}");
                 var requestAdd = new RestRequest(Method.POST);
                 requestAdd.AddHeader("Content-Type", "application/json");
-                requestAdd.AddParameter("application/json", $"{{\n\t\"id\" : {todoMethod[2]}, \n\t\"todo\" : \"spm\"\n, \n\t\"done\" : true\n}}", ParameterType.RequestBody);
+                requestAdd.AddParameter("application/json", $"{{\n\t\"id\" : {todoMethod[2]}, \n\t\"todo\" : \"{content}\"\n, \n\t\"done\" : true\n}}", ParameterType.RequestBody);
                 var response1 = client.Execute(requestAdd);
                 Console.WriteLine(response1.Content);
             }
             else if (todoMethod[1].ToLower() == "undone")
             {
+                var requestList = new RestRequest(Method.GET);
+                requestList.AddHeader("Content-Type", "list/json");
+                var response = client.Execute(requestList);
+                var jsonFile = JsonConvert.DeserializeObject<List<Todo>>(response.Content);
+
+                var content = "";
+
+                foreach (var i in jsonFile)
+                {
+                    if (i.id.ToString() == todoMethod[2]) { content += i.todo; break; }
+                }
+
                 var client1 = new RestClient($"http://localhost:3000/todo/{todoMethod[2]}");
                 client1.Timeout = -1;
+
                 var request = new RestRequest(Method.DELETE);
                 request.AddHeader("Content-Type", "application/json");
-                IRestResponse response = client1.Execute(request);
+                client1.Execute(request);
 
-                var content = JsonConvert.DeserializeObject<List<Todo>>(response.Content)[0].todo;
                 Console.WriteLine($"{{\n\t\"todo\" : \"{content}\"\n}}");
                 var requestAdd = new RestRequest(Method.POST);
                 requestAdd.AddHeader("Content-Type", "application/json");
