54cfbd09dc6c94cca99ea00a0b1291a3e7d61a24
Gigaming Intermedia Solusindo <abdul.salim@gigaming.io>
2020-03-02 16:34:30 +0700

    Done

--- Week5FP/Week5FP/Program.cs
+++ Week5FP/Week5FP/Program.cs
@@ -9,17 +9,19 @@ namespace Week5FP
         static void Main(string[] args)
         {
             var str = "hello world again";
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 6, 8, 8, 6, 9 };
+            var grades = new[] { 87.5, 88.5, 60.5, 90.5, 88.5 };
 
             var cap = str.Capitalize();
             var snake = str.SnakeCase();
             var kebab = str.KebabCase();
-
-            var groupby = new [] { 1, 2, 3, 3, 2, 3, 4, 1 };
-            var newg = groupby.Mode();
-
-            var trim = str.Trim(8);
-
-            var trimw = str.TrimWords(2);
+            var modeNumber = numbers.Mode();
+            var modeGrades = grades.Mode();
+            var numberToString1 = 1.intToString();
+            var numberToString2 = 12.intToString();
+            var numberToString3 = 30.intToString();
+            var tulisanPanjang = "ini adalah tulisan yang sangat panjang".Trim(8);
+            var tulisanPanjangAgain = "ini adalah tulisan yang sangat panjang".TrimWords(3);
 
             Console.WriteLine();
         }
@@ -39,14 +41,19 @@ namespace Week5FP
               .Select(s => new { key = s.Key, count = s.Count() })
               .OrderByDescending(x => x.count).First().key;
 
-        //public static string intToString(int i)
-        //{
+        public static string intToString(this int i)
+        {
+            var words = "Satu Dua Tiga Empat Lima Enam Tujuh Delapan Sembilan Sepuluh Belas".Split(' ').ToList();
 
-        //    return "satu";
-        //}
+            return (i <= 10) ? words[i - 1] :
+                    ((i < 20) ?  words[i%10 - 1]+" "+words[10] :
+                        words[i/10 - 1]+" puluh"+((i%10 != 0) ? " "+words[i % 10 - 1] : "") ) ;
+        }
 
         public static string Trim(this string s1, int i) => s1.Substring(0, i) + "...";
 
         public static string TrimWords(this string s1, int i) => string.Join(" ", s1.Split(' ').ToList().GetRange(0, i));
+
+
     }
 }
